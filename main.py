2921fcac229a7abf99373042cd306b48a86f38f4
Mahdi Taheri <151569179+MhdiTaheri@users.noreply.github.com>
2023-12-18 19:27:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import shutil
+from datetime import datetime
+import urllib.parse
+
 
 def get_v2ray_links(url):
     response = requests.get(url)
@@ -53,22 +56,78 @@ def save_configs_by_region(configs):
         for folder in os.listdir(config_folder):
             folder_path = os.path.join(config_folder, folder)
             if os.path.isdir(folder_path):
-                shutil.rmtree(folder_path)  # Remove all subdirectories
+                shutil.rmtree(folder_path)
 
     if not os.path.exists(config_folder):
         os.makedirs(config_folder)
 
     for config in configs:
-        ip = config.split('//')[1].split('/')[0]  # Extracting IP from config URL
+        ip = config.split('//')[1].split('/')[0]
         region = get_region_from_ip(ip)
         if region:
             region_folder = os.path.join(config_folder, region)
             if not os.path.exists(region_folder):
                 os.makedirs(region_folder)
 
-            with open(os.path.join(region_folder, 'config.txt'), 'a') as file:
+            with open(os.path.join(region_folder, 'config.txt'), 'a', encoding='utf-8') as file:
                 file.write(config + '\n')
 
+
+
+def update_readme():
+    readme_path = "README.md"
+    timestamp = f"{datetime.now().strftime('%a-%d-%b-%Y %H:%M')}"
+
+    with open(readme_path, 'r', encoding='utf-8') as readme_file:
+        content = readme_file.read()
+
+    emoji_position = content.find('📅')
+
+    if emoji_position != -1:
+        timestamp_start = emoji_position + len('📅')
+        timestamp_end = content.find('\n', timestamp_start)
+
+        if timestamp_start != -1 and timestamp_end != -1:
+            existing_timestamp = content[timestamp_start:timestamp_end]
+            updated_content = content.replace(f"{existing_timestamp}", f":`{timestamp}`")
+        else:
+            updated_content = content[:emoji_position + len('📅')] + f" `{timestamp}`" + content[emoji_position + len('📅'):]
+    else:
+        updated_content = content + f"\n\n📅 {timestamp}\n"
+
+    with open(readme_path, 'w', encoding='utf-8') as readme_file:
+        readme_file.write(updated_content)
+
+
+def create_sub_section():
+    readme_path = "README.md"
+    sub_folder = "sub"
+    found_sub_section = False
+
+    if os.path.exists(readme_path):
+        with open(readme_path, 'r', encoding='utf-8') as readme_file:
+            content = readme_file.read()
+
+            if '## Sub' in content:
+                found_sub_section = True
+
+    new_content = ""
+    new_content += "## Sub\n"
+    new_content += "| Sub |\n"
+    new_content += "|-----|\n"
+
+    for root, dirs, files in os.walk(sub_folder):
+        for directory in dirs:
+            config_path = os.path.join(root, directory, 'config.txt')
+            if os.path.exists(config_path):
+                url = f"https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector_Py/main/sub/{urllib.parse.quote(directory)}/config.txt"
+                new_content += f"| [{directory}]({url}) |\n"
+
+    with open(readme_path, 'w', encoding='utf-8') as readme_file:
+        if found_sub_section:
+            readme_file.write(content.replace(content[content.find('## Sub'):content.find('\n\n', content.find('## Sub'))], new_content))
+        else:
+            readme_file.write(content + new_content)
 
 if __name__ == "__main__":
     telegram_urls = [
@@ -174,6 +233,8 @@ if __name__ == "__main__":
 
     if all_v2ray_configs:
         save_configs_by_region(all_v2ray_configs)
+        update_readme()
+        create_sub_section()
         print("Configs saved successfully.")
     else:
         print("No V2Ray configs found.")
